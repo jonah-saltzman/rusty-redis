a389259f4d9efa89277722d4a55b3ca0abefa8a7
Jonah Saltzman <jonahsaltzman@gmail.com>
2023-05-22 18:20:45 -0400

    async

--- src/bin/server/main.rs
+++ src/bin/server/main.rs
@@ -1,12 +1,12 @@
+use tokio::net::{TcpStream, TcpListener};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use anyhow::{Context, Error, Result};
-use std::io::{self, Read, Write};
-use std::net::{TcpListener, TcpStream};
-use std::thread;
+use std::io;
 use log::{debug, info, trace};
 
 const K_MAX_MSG: usize = 4096 + 1;
 
-fn read_bytes(stream: &mut TcpStream, bytes: usize) -> Result<Vec<u8>> {
+async fn read_bytes(stream: &mut TcpStream, bytes: usize) -> Result<Vec<u8>> {
     debug!("reading {} bytes from stream", bytes);
     let mut buf = vec![0u8; bytes];
     let mut bytes_read: usize = 0;
@@ -14,6 +14,7 @@ fn read_bytes(stream: &mut TcpStream, bytes: usize) -> Result<Vec<u8>> {
         trace!("bytes_read: {}", bytes_read);
         let bytes = stream
             .read(&mut buf[bytes_read..])
+            .await
             .context("failed to read bytes from tcpstream")?;
         if bytes == 0 {
             return Err(io::Error::new(
@@ -27,7 +28,7 @@ fn read_bytes(stream: &mut TcpStream, bytes: usize) -> Result<Vec<u8>> {
     Ok(buf)
 }
 
-fn write_bytes<T: AsRef<[u8]>>(stream: &mut TcpStream, buf: &T) -> Result<()> {
+async fn write_bytes<T: AsRef<[u8]>>(stream: &mut TcpStream, buf: &T) -> Result<()> {
     let slice: &[u8] = buf.as_ref();
     debug!("writing {} bytes to stream", slice.len());
     let mut bytes_written: usize = 0;
@@ -35,41 +36,46 @@ fn write_bytes<T: AsRef<[u8]>>(stream: &mut TcpStream, buf: &T) -> Result<()> {
         trace!("bytes_written: {}", bytes_written);
         let bytes = stream
             .write(&slice[bytes_written..])
+            .await
             .context("failed to write bytes to the stream")?;
         bytes_written += bytes;
     }
     Ok(())
 }
 
-fn handle_client(mut stream: TcpStream) -> Result<()> {
-    let header = read_bytes(&mut stream, 4).context("read header")?;
+async fn handle_client(mut stream: TcpStream) -> Result<()> {
+    let header = read_bytes(&mut stream, 4).await.context("read header")?;
     let len = u32::from_le_bytes(header.try_into().unwrap());
-    info!("new message of len {}", len);
+    debug!("new message of len {}", len);
     if len > K_MAX_MSG as u32 {
         return Err(Error::msg(
             "received req with len header greater than max size",
         ));
     }
-    let mut bytes = read_bytes(&mut stream, len.try_into().unwrap()).context("read body")?;
+    let mut bytes = read_bytes(&mut stream, len.try_into().unwrap()).await.context("read body")?;
     let message = std::str::from_utf8_mut(&mut bytes).context("request not valid utf8")?;
     let response = format!("ECHO: {}", message);
     let response_header = (response.len() as u32).to_le_bytes();
-    info!("writing response of len {}", response.len());
-    write_bytes(&mut stream, &response_header).context("write header")?;
-    write_bytes(&mut stream, &response).context("write body")?;
-    info!("wrote response");
+    debug!("writing response of len {}", response.len());
+    write_bytes(&mut stream, &response_header).await.context("write header")?;
+    write_bytes(&mut stream, &response).await.context("write body")?;
     Ok(())
 }
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     env_logger::builder().is_test(true).init();
-    let listener = TcpListener::bind("0.0.0.0:6379").context("failed to bind address")?;
-    for stream in listener.incoming() {
-        let stream = stream.context("failed to open stream")?;
+    let listener = TcpListener::bind("0.0.0.0:6379").await.context("failed to bind address")?;
+    loop {
+        let (stream, _) = listener.accept().await.context("failed to accept incoming connection")?;
         info!("New connection: {}", stream.peer_addr().unwrap());
-        thread::spawn(move || handle_client(stream));
+        tokio::spawn(async move {
+            let r = handle_client(stream).await;
+            if let Err(e) = r {
+                eprintln!("Error handling client: {:?}", e);
+            }
+        });
     }
-    Ok(())
 }
 
 #[cfg(test)]
@@ -102,7 +108,7 @@ mod tests {
             let response_len = u32::from_le_bytes(header);
             let mut response = vec![0; response_len as usize];
             stream.read_exact(&mut response).unwrap();
-            
+
             let response = String::from_utf8(response).unwrap();
 
             assert_eq!(response, format!("ECHO: {}", request));
